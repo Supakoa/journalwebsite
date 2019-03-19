733e091726bc57870a1e16c132a6f85ba3463a84
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2019-03-19 15:26:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta http-equiv="refresh" content="0;url=f-end/main.php">
 <title>WEBSITE JOURNAL</title>
 <script language="javascript">
-    window.location.href = "f-end/index.php"
+    window.location.href = "f-end/main.php"
 </script>
 </head>
 <body>
